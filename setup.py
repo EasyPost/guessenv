bc4bebcc4d1730c72a1fd998491801ec216a8771
James Brown <jbrown@easypost.com>
2019-02-07 17:31:36 -0800

    populate some more setup.py fields

--- setup.py
+++ setup.py
@@ -15,10 +15,15 @@ setup(
     license='ISC',
     packages=find_packages(exclude=['test']),
     description='Parse Python files and look for required environment variables',
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
     entry_points={
         'console_scripts': [
             'guessenv = guessenv.__main__:main',
         ]
+    },
+    project_urls={
+        'Issue Tracker': 'https://github.com/easypost/guessenv/issues',
     },
     classifiers=[
         'Development Status :: 3 - Alpha',
